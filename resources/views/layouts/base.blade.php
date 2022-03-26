6ca5003bf88768dabfb31aa11d30ab1c8a4445ff
VaishnoS <vaishnoprakash1@gmail.com>
2022-03-26 22:04:29 +0530

    26-03-2022 Second Commit

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -15,6 +15,7 @@
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/chosen.min.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/color-01.css') }}">
+	<!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
     @livewireStyles
 </head>
 <body class="home-page home-01 ">
@@ -42,8 +43,6 @@
 						</div>
 						<div class="topbar-menu right-menu">
 							<ul>
-								<li class="menu-item" ><a title="Register or Login" href="login.html">Login</a></li>
-								<li class="menu-item" ><a title="Register or Login" href="register.html">Register</a></li>
 								<li class="menu-item lang-menu menu-item-has-children parent">
 									<a title="English" href="#"><span class="img label-before"><img src="{{ asset('assets/images/lang-en.png') }}" alt="lang-en"></span>English<i class="fa fa-angle-down" aria-hidden="true"></i></a>
 									<ul class="submenu lang" >
@@ -67,6 +66,32 @@
 										</li>
 									</ul>
 								</li>
+								@if(Route::has('login'))
+									@auth
+										@if(Auth::user()->utype==='ADM')
+										<li class="menu-item menu-item-has-children parent" >
+											<a title="My Account" href="#">My Account ({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+											<ul class="submenu curency" >
+												<li class="menu-item" >
+													<a title="Dashboard" href="#">Dashboard</a>
+												</li>
+											</ul>
+										</li>
+										@else
+										<li class="menu-item menu-item-has-children parent" >
+											<a title="My Account" href="#">My Account ({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+											<ul class="submenu curency" >
+												<li class="menu-item" >
+													<a title="Dashboard" href="#">Dashboard</a>
+												</li>
+											</ul>
+										</li>
+										@endif
+									@else
+										<li class="menu-item" ><a title="Register or Login" href="{{route('login')}}">Login</a></li>
+										<li class="menu-item" ><a title="Register or Login" href="{{route('register')}}">Register</a></li>
+									@endif
+								@endif	
 							</ul>
 						</div>
 					</div>
@@ -159,19 +184,19 @@
 						<div class="container">
 							<ul class="nav primary clone-main-menu" id="mercado_main" data-menuname="Main menu" >
 								<li class="menu-item home-icon">
-									<a href="index.html" class="link-term mercado-item-title"><i class="fa fa-home" aria-hidden="true"></i></a>
+									<a href="{{ url('/')}}" class="link-term mercado-item-title"><i class="fa fa-home" aria-hidden="true"></i></a>
 								</li>
 								<li class="menu-item">
 									<a href="about-us.html" class="link-term mercado-item-title">About Us</a>
 								</li>
 								<li class="menu-item">
-									<a href="shop.html" class="link-term mercado-item-title">Shop</a>
+									<a href="{{url('/shop')}}" class="link-term mercado-item-title">Shop</a>
 								</li>
 								<li class="menu-item">
-									<a href="cart.html" class="link-term mercado-item-title">Cart</a>
+									<a href="{{url('/cart')}}" class="link-term mercado-item-title">Cart</a>
 								</li>
 								<li class="menu-item">
-									<a href="checkout.html" class="link-term mercado-item-title">Checkout</a>
+									<a href="{{url('/checkout')}}" class="link-term mercado-item-title">Checkout</a>
 								</li>
 								<li class="menu-item">
 									<a href="contact-us.html" class="link-term mercado-item-title">Contact Us</a>
